6cfc5d6280b787fcf4109c9e9e093c5e84285ba2
Barbara Santos <barbaransantos1@gmail.com>
2021-10-09 17:19:47 -0300

    Rails Model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,42 @@
 class TasksController < ApplicationController
+  def index
+    raise
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new(params[:tasks])
+    @task.save
+  end
+
+  def create
+    @task = Task.new(params[:tasks])
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Tasks.find(params[:id])
+    @task.update(tasks_params)
+
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :detail, :completed)
+  end
 end
